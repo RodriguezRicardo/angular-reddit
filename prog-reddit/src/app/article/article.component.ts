66efa92a49e90f6fdc96eeaa5cce86acf96b9b59
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2020-01-22 13:58:14 +0000

    multiple articles

--- prog-reddit/src/app/article/article.component.ts
+++ prog-reddit/src/app/article/article.component.ts
@@ -1,32 +1,32 @@
 import { Component, OnInit, HostBinding} from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'card';
-  votes: number;
-  title: string;
-  link: string;
+  article:Article
+
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article('Angular 2', 'http://angular.io', 10);
   }
 
   voteUp():Boolean { //Aggiunto tipo di ritorno
-    this.votes += 1;
+    this.article.voteUp();
     return false;  //Non propagare l'evento
   }
 
   voteDown():Boolean  {
-    this.votes -= 1;
+    this.article.voteDown();
     return false;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
+
+
